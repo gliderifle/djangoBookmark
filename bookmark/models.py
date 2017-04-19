2f03583b8c755a9ca05cbd750f71d0b9aac84009
gliderifle <gliderifle@redtable.kr>
2017-04-19 15:36:16 +0900

    Bookmark admin에 추가

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Bookmark(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=100, blank=True)
+    
+    def __str__(self):
+        return self.title
