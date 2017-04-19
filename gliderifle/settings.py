a31ececece0c3502c387cec7d7996a418fd4cb80
gliderifle <gliderifle@redtable.kr>
2017-04-19 15:21:55 +0900

    Change setting

--- gliderifle/settings.py
+++ gliderifle/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'gliderifle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'gliderifle', 's')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,7 +92,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -102,8 +104,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/' #url prefix
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gliderifle', 'static'),
+]
 
-MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/' #url prefix
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
